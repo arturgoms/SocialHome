a3410701bb38d04bb7c8f218340fd197675dc223
Artur Gomes <flashballclube@gmail.com>
2016-11-25 15:19:52 -0200

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import app
 from app import INI
 from app import BASE_DIR
-
+from gevent.wsgi import WSGIServer
 
 if __name__ == '__main__':
-    app.run(debug=INI[0], host=INI[1], port=INI[2])
+    #app.run(debug=INI[0], host=INI[1], port=INI[2])
+    http_server = WSGIServer(('', port=INI[2]), app)
+    http_server.serve_forever()
